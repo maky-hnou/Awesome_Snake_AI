e39c1a409f8eca431553392d45e9aa4779da0cec
hani <haniyousfi@gmail.com>
2020-10-18 13:26:04 +0100

    Set info zone in the screen

--- scratch/play.py
+++ scratch/play.py
@@ -12,12 +12,13 @@ if __name__ == '__main__':
     black = (0, 0, 0)
     width = 600
     height = 400
+    info_zone = 60
     block = 20
     pink = (171, 54, 81)
     blue = (106, 133, 164)
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((width, height + info_zone))
     # Create snake
-    sn = Snake(width=width, height=height, block=block)
+    sn = Snake(width=width, height=height, block=block, info_zone=info_zone)
     x_snake, y_snake = sn.get_random_location()
     snake = [(x_snake, y_snake)]
     direction = ''
@@ -26,15 +27,16 @@ if __name__ == '__main__':
     while True:
         # Draw scene
         gr = Graphic(scene_width=width, scene_height=height,
-                     block=block, bg_color=bg, food_color=pink,
-                     wall_color=black, snake_color=blue)
+                     info_zone=info_zone, block=block, bg_color=bg,
+                     food_color=pink, wall_color=black, snake_color=blue)
 
-        scene = gr.draw_scene(scene=screen)
+        scene = gr.draw_scene(scene=screen, score=score)
         if eaten:
             food = ()
             while food == ():
                 x_food = random.randint(3 * block, width - 2 * block)
                 y_food = random.randint(3 * block, height - 2 * block)
+                + info_zone
                 x_food = block * round(x_food / block)
                 y_food = block * round(y_food / block)
                 if (x_food, y_food) not in snake:
@@ -60,7 +62,7 @@ if __name__ == '__main__':
         snake, eaten, score = sn.move_snake(
             snake=snake, direction=direction, food=food, score=score)
         if sn.is_dead(snake=snake, direction=direction):
-            print('died\nscore = ', score)
+            print('Died\nscore = ', score)
             pygame.quit()
             sys.exit(0)
 
