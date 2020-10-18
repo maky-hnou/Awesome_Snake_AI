7b85b5dcb7905fe82e4a956d9b3440b7ad7745e5
hani <haniyousfi@gmail.com>
2020-10-18 13:26:37 +0100

    Set the graphics of the info zone and text

--- scratch/graphic.py
+++ scratch/graphic.py
@@ -3,7 +3,7 @@ import pygame
 
 class Graphic:
     def __init__(self, scene_width, scene_height, block, bg_color,
-                 food_color, wall_color, snake_color):
+                 food_color, wall_color, snake_color, info_zone):
 
         self.scene_width = scene_width
         self.scene_height = scene_height
@@ -11,20 +11,28 @@ class Graphic:
         self.bg_color = bg_color
         self.food_color = food_color
         self.wall_color = wall_color
+        self.info_zone = info_zone
 
         self.snake_color = snake_color
+        # Init the font
+        pygame.font.init()
+        self.myfont = pygame.font.Font('freesansbold.ttf', 25)
 
-    def draw_scene(self, scene):
+    def draw_scene(self, scene, score):
         # Fill the background
         scene.fill(self.bg_color)
+        # set the text to be written
+        textsurface = self.myfont.render('Score: {}'.format(score),
+                                         False, (55, 55, 55))
+        scene.blit(textsurface, (20, 20))
         # building the horizontal walls
         for x in range(0, self.scene_width, self.block):
-            y = 0
+            y = self.info_zone
             pygame.draw.rect(scene, self.wall_color,
                              (x, y, self.block, self.block), 1)
             pygame.draw.rect(scene, self.wall_color,
                              (x+3, y+3, self.block-6, self.block-6))
-            y = self.scene_height - self.block
+            y = self.scene_height - self.block + self.info_zone
             pygame.draw.rect(scene, self.wall_color,
                              (x, y, self.block, self.block), 1)
             pygame.draw.rect(scene, self.wall_color,
@@ -33,14 +41,20 @@ class Graphic:
         for y in range(self.block, self.scene_height-self.block, self.block):
             x = 0
             pygame.draw.rect(scene, self.wall_color,
-                             (x, y, self.block, self.block), 1)
+                             (x, y + self.info_zone,
+                              self.block, self.block),
+                             1)
             pygame.draw.rect(scene, self.wall_color,
-                             (x+3, y+3, self.block-6, self.block-6))
+                             (x+3, y+3 + self.info_zone,
+                              self.block-6, self.block-6))
             x = self.scene_width - self.block
             pygame.draw.rect(scene, self.wall_color,
-                             (x, y, self.block, self.block), 1)
+                             (x, y + self.info_zone,
+                              self.block, self.block),
+                             1)
             pygame.draw.rect(scene, self.wall_color,
-                             (x+3, y+3, self.block-6, self.block-6))
+                             (x+3, y+3 + self.info_zone,
+                              self.block-6, self.block-6))
         return scene
 
     def draw_food(self, scene, food):
