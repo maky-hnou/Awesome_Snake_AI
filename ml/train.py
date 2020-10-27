5d1f7b9ed3e752c7c93c4c258664e4695bde84e5
hani <haniyousfi@gmail.com>
2020-10-27 23:39:10 +0100

    Edit training code

--- ml/train.py
+++ ml/train.py
@@ -9,27 +9,19 @@ class TrainSnake:
         self.train_y = train_y
 
     def create_training_model(self):
-        # model = tf.keras.Sequential()
-        # model.add(tf.keras.layers.Dense(units=9, input_dim=7))
-        #
-        # model.add(tf.keras.layers.Dense(units=15, activation='relu'))
-        # model.add(tf.keras.layers.Dense(units=1, activation='softmax'))
-        #
-        # model.compile(loss='mean_squared_error', optimizer='adam',
-        #               metrics=['accuracy'])
-        # print(model.summary)
-        model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Dense(units=25, activation='relu',
-                                        input_shape=(7, 1)))
-        model.add(tf.keras.layers.Dense(units=1, activation='linear'))
-        opt = tf.keras.optimizers.Adam(learning_rate=0.01, beta_1=0.9,
-                                       beta_2=0.999, epsilon=1e-07,
-                                       amsgrad=False, name='Adam')
-        model.compile(optimizer=opt, loss='mse')
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(units=9, input_dim=7))
+        model.add(tf.keras.layers.Dense(units=15, activation='relu'))
+        model.add(tf.keras.layers.Dense(units=3,  activation='softmax'))
+
+        model.compile(loss='mean_squared_error',
+                      optimizer='adam',
+                      metrics=['accuracy'])
         print(model.summary())
-        model.fit((np.array(self.train_x).reshape(-1, 7, 1)),
-                  (np.array(self.train_y).reshape(-1, 3, 1)),
-                  batch_size=256, epochs=300)
+        model.fit((np.array(self.train_x).reshape(-1, 7)),
+                  (np.array(self.train_y).reshape(-1, 3)),
+                  batch_size=256,
+                  epochs=30)
 
         model.save_weights('model.h5')
         model_json = model.to_json()
