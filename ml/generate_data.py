8b6c9d226449cbcd8a331c74ca1077de3f626afe
hani <haniyousfi@gmail.com>
2020-11-28 20:13:11 +0100

    Add Docstrings to GenerateData class

--- ml/generate_data.py
+++ ml/generate_data.py
@@ -7,6 +7,43 @@ from utils import Utils
 
 
 class GenerateData:
+    """Class to simulate games and generate the training data.
+
+    Parameters
+    ----------
+    width : int
+        The width of the scene.
+    height : int
+        The height of the scene.
+    block : int
+        The size of the blocks forming the snake and the walls.
+    info_zone : int
+        The info zone height.
+    clock_rate : int
+        The speed of the snake.
+
+    Attributes
+    ----------
+    training_data_x : numpy ndarray
+        The generated training data.
+    training_data_y : numpy ndarray
+        The training data labels.
+    training_games : int
+        The number of the games to be simulated.
+    steps_per_game : int
+        The number of moves per games.
+    sn : class
+        The class to create the snake.
+    utils : class
+        The class with helper methods.
+    height
+    width
+    block
+    info_zone
+    clock_rate
+
+    """
+
     def __init__(self, height, width, block, info_zone, clock_rate):
         self.height = height
         self.width = width
@@ -22,6 +59,16 @@ class GenerateData:
                            block=self.block, info_zone=self.info_zone)
 
     def generate_training_data(self):
+        """Generate training data.
+
+        Returns
+        -------
+        training_data_x: numpy ndarray
+            The training data.
+        training_data_y: numpy ndarray
+            The training data labels.
+
+        """
         screen = pygame.display.set_mode((self.width, self.height))
 
         for game in tqdm(range(self.training_games)):
@@ -59,6 +106,23 @@ class GenerateData:
         return self.training_data_x, self.training_data_y
 
     def generate_random_direction(self, snake, food_angle):
+        """Generate random direction.
+
+        Parameters
+        ----------
+        snake : tuple[int]
+            The snake coordinates.
+        food_angle : float
+            The angle between the food and the snake's head.
+
+        Returns
+        -------
+        direction: int
+            The the next snake direction.
+        button_direction: int
+            The direction that corresponds to the next direction.
+
+        """
         direction = 0
         if food_angle > 0:
             direction = 1
@@ -70,6 +134,25 @@ class GenerateData:
         return self.direction_vector(snake, food_angle, direction)
 
     def direction_vector(self, snake, food_angle, direction):
+        """Generate a coordinates vector based on the gived direction.
+
+        Parameters
+        ----------
+        snake : tuple[int]
+            The snake coordinates.
+        food_angle : float
+            The angle between the food and the snake's head.
+        direction : int
+            The generated direction.
+
+        Returns
+        -------
+        direction: int
+            The the next snake direction.
+        button_direction: int
+            The direction that corresponds to the next direction.
+
+        """
         current_direction = np.array(snake[0]) - np.array(snake[1])
         left_direction = np.array([current_direction[1],
                                    -current_direction[0]])
@@ -90,6 +173,38 @@ class GenerateData:
     def generate_training_data_y(self, snake, food_angle, button_direction,
                                  direction, training_data_y, is_front_blocked,
                                  is_left_blocked, is_right_blocked):
+        """Generate the training data labels.
+
+        Parameters
+        ----------
+        snake : tuple[int]
+            The snake coordinates.
+        food_angle : float
+            The angle between the food and the snake's head.
+
+        button_direction : int
+            The direction that corresponds to the next direction.
+        direction : int
+            The generated direction.
+        training_data_y : numpy ndarray
+            The training data labels.
+        is_front_blocked : bool
+            True if the front is blocked, False if not.
+        is_left_blocked : bool
+            True if the left is blocked, False if not.
+        is_right_blocked : bool
+            True if the right is blocked, False if not.
+
+        Returns
+        -------
+        direction : int
+            The generated direction.
+        button_direction : int
+            The direction that corresponds to the next direction.
+        training_data_y : numpy ndarray
+            The training data labels.
+
+        """
         if direction == -1:
             if is_left_blocked == 1:
                 if is_front_blocked == 1 and is_right_blocked == 0:
